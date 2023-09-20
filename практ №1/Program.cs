682a99319d605df1cc4fef5bdab7d9c30dfbeea4
89162 <89162@DESKTOP-100SLPA>
2023-09-20 23:25:04 +0300

    Добавьте файлы проекта.

---
+++ практ №1/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+char again = '1';
+while (again == '1')
+{
+    double a;
+    double b;
+    double total;
+    char oper;
+
+    Console.WriteLine("Введите первое число:");
+    a = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine("Введите оператор:");
+    oper = Convert.ToChar(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число:");
+    b = Convert.ToDouble(Console.ReadLine());
+
+    if (oper == '+')
+    {
+        total = a + b;
+        Console.WriteLine(total);
+    }
+
+    else if (oper == '-')
+    {
+        total = a - b;
+        Console.WriteLine(total);
+    }
+
+    else if (oper == '*')
+    {
+        total = a * b;
+        Console.WriteLine(total);
+    }
+
+    else if (oper == '/')
+    {
+        total = a / b;
+        Console.WriteLine(total);
+    }
+    else if (oper == '%')
+    {
+        total = a / 100; 
+        Console.WriteLine(total);
+    }
+    else if (oper == '^')
+    {
+        int x = (int)a;
+        int y = (int)b;
+        total = x ^ y;
+        Console.WriteLine(total);
+    }
+    else
+    {
+        Console.WriteLine("Неизвестный оператор.");
+    }
+    Console.WriteLine("Вы хотите продолжить работу с калькулятором? (1 = да, 2 = нет)");
+    again = Convert.ToChar(Console.ReadLine());
+}
